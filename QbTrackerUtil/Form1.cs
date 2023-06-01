9c6f2d12c4eb9b6e3c415d88741074fda2e3e151
shabby2333 <shabby2333@qq.com>
2023-06-01 20:23:12 +0800

    添加项目文件。

---
+++ QbTrackerUtil/Form1.cs
@@ -0,0 +1,91 @@
+﻿using QBittorrent.Client;
+using System;
+using System.Windows.Forms;
+
+namespace QbTrackerUtil
+{
+    public partial class Form1 : Form
+    {
+
+        private QBittorrentClient _client = null;
+        private bool _login = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            _client = new QBittorrentClient(new Uri(tbQbUrl.Text));
+            try
+            {
+                await _client.LoginAsync(tbUsername.Text, tbPassword.Text);
+                _login = true;
+
+                Invoke(new Action(() =>
+                {
+                    btnReplace.Enabled = true;
+                    tbLog.AppendText("登录成功\n");
+                }));
+            }
+            catch (Exception ex)
+            {
+                Invoke(new Action(() =>
+                {
+                    _login = false;
+                    btnReplace.Enabled = false;
+                    MessageBox.Show(ex.Message);
+                }));
+            }
+
+        }
+
+        private async void btnReplace_Click(object sender, EventArgs e)
+        {
+            var passkey = tbPasskey.Text;
+
+            var torrentList = await _client.GetTorrentListAsync();
+            Invoke(new Action(() =>
+            {
+                tbLog.AppendText($"请求种子列表成功，共{torrentList.Count}个种子\n");
+            }));
+            var successCount = 0;
+            var failCount = 0;
+            foreach (var torrent in torrentList)
+            {
+                var trackerList = await _client.GetTorrentTrackersAsync(torrent.Hash);
+                foreach (var tracker in trackerList)
+                {
+                    if (tracker.Url.IsAbsoluteUri && tracker.Url.Host.Contains("hdtime.org"))
+                    {
+                        try
+                        {
+                            await _client.EditTrackerAsync(torrent.Hash, tracker.Url, new Uri($"https://tracker.hdtime.org/announce.php?passkey={passkey}"));
+
+                            successCount++;
+                            Invoke(new Action(() =>
+                            {
+                                tbLog.AppendText($"将[{torrent.Name}]中的tracker[{tracker.Url}]替换成功\n");
+                            }));
+                        }
+                        catch
+                        {
+                            failCount++;
+                            Invoke(new Action(() =>
+                            {
+                                tbLog.AppendText($"将[{torrent.Name}]中的tracker[{tracker.Url}]替换失败\n");
+                            }));
+                        }
+
+                    }
+                }
+            }
+
+            Invoke(new Action(() =>
+            {
+                tbLog.AppendText($"替换完成,成功{successCount}个，失败{failCount}个\n");
+            }));
+        }
+    }
+}
---
+++ TrTrackerUtil/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace QbTrackerUtil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.tbLog = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tbQbUrl = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.tbUsername = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.tbPassword = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnLogin = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
+            this.tbPasskey = new System.Windows.Forms.ToolStripTextBox();
+            this.btnReplace = new System.Windows.Forms.ToolStripButton();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            this.toolStripContainer1.BottomToolStripPanelVisible = false;
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.tbLog);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(608, 356);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.LeftToolStripPanelVisible = false;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.RightToolStripPanelVisible = false;
+            this.toolStripContainer1.Size = new System.Drawing.Size(608, 406);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip2);
+            // 
+            // tbLog
+            // 
+            this.tbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbLog.Location = new System.Drawing.Point(0, 0);
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            this.tbLog.Size = new System.Drawing.Size(608, 356);
+            this.tbLog.TabIndex = 0;
+            this.tbLog.Text = "";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.tbQbUrl});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(461, 25);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(147, 22);
+            this.toolStripLabel1.Text = "transmission web ui地址";
+            // 
+            // tbQbUrl
+            // 
+            this.tbQbUrl.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.tbQbUrl.Name = "tbQbUrl";
+            this.tbQbUrl.Size = new System.Drawing.Size(300, 25);
+            this.tbQbUrl.Text = "http://192.168.0.4:9091/transmission/rpc";
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel2,
+            this.tbUsername,
+            this.toolStripSeparator1,
+            this.toolStripLabel3,
+            this.tbPassword,
+            this.toolStripSeparator2,
+            this.btnLogin,
+            this.toolStripSeparator3,
+            this.toolStripSeparator4,
+            this.toolStripLabel4,
+            this.tbPasskey,
+            this.btnReplace});
+            this.toolStrip2.Location = new System.Drawing.Point(3, 25);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(545, 25);
+            this.toolStrip2.TabIndex = 1;
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(44, 22);
+            this.toolStripLabel2.Text = "用户名";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 25);
+            this.tbUsername.Text = "admin";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(32, 22);
+            this.toolStripLabel3.Text = "密码";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
+            this.btnLogin.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(36, 22);
+            this.btnLogin.Text = "登录";
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel4
+            // 
+            this.toolStripLabel4.Name = "toolStripLabel4";
+            this.toolStripLabel4.Size = new System.Drawing.Size(55, 22);
+            this.toolStripLabel4.Text = "passkey";
+            // 
+            // tbPasskey
+            // 
+            this.tbPasskey.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.tbPasskey.Name = "tbPasskey";
+            this.tbPasskey.Size = new System.Drawing.Size(100, 25);
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnReplace.Enabled = false;
+            this.btnReplace.Image = ((System.Drawing.Image)(resources.GetObject("btnReplace.Image")));
+            this.btnReplace.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(36, 22);
+            this.btnReplace.Text = "替换";
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 406);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox tbQbUrl;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripTextBox tbUsername;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.ToolStripTextBox tbPassword;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnLogin;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel4;
+        private System.Windows.Forms.ToolStripTextBox tbPasskey;
+        private System.Windows.Forms.ToolStripButton btnReplace;
+        private System.Windows.Forms.RichTextBox tbLog;
+    }
+}
+
